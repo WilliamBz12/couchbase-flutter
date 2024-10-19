fddeb43905496a2bf87ed9b64a5082ba440752a3
Williambz12 <williamsilva754@gmail.com>
2024-10-19 11:26:19 -0300

    video 1.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:cbl_flutter/cbl_flutter.dart';
 import 'package:flutter/material.dart';
 
 import 'app/app_widget.dart';
 
-void main() {
+Future<void> main() async {
+  // Initialize Flutter before Couchbase Lite.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Now initialize Couchbase Lite.
+  await CouchbaseLiteFlutter.init();
+
+  // Finally, start running the app.
   runApp(const MyApp());
 }
